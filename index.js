90ebb7c088c8eaad148d3e6c9601da0e3f642a88
Olena Vdovych <olena.vdovych2@gmail.com>
2023-03-10 22:28:49 +0200

    Added example 2

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // Example 1 
-let incomePayment = Number(prompt('Введіть бажаний дохід :'));
+let incomePayment = Number(prompt('Введіть бажаний дохід:'));
 let lateness = Number(prompt('Введіть кількість запізнень:'));
 let linesOfCode = Math.floor((incomePayment / 50) * 100);
 if (lateness >= 3) {
     linesOfCode -= (lateness - 2) * 50;
 }
 alert(`Вам потрібно написати ${linesOfCode} рядків коду`);
+
+// Example 2
+let writtenLines = Number(prompt('Введіть кількість написаних рядків коду:'));
+let desiredIncome = Number(prompt('Введіть бажаний дохід:'));
+let remainingLines = Math.floor((desiredIncome / 50) * 100) - writtenLines;
+let remainingLateness = Math.ceil((writtenLines / 100) / 3) - 1;
+alert (`Ви можете запізнитися ще ${remainingLateness} разів`);
