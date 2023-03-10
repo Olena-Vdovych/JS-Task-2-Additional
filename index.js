31084618a3b1db8271be978063402dcfc7ad60bf
Olena Vdovych <olena.vdovych2@gmail.com>
2023-03-10 22:41:32 +0200

    Added example 3

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ let desiredIncome = Number(prompt('Введіть бажаний дохід:'));
 let remainingLines = Math.floor((desiredIncome / 50) * 100) - writtenLines;
 let remainingLateness = Math.ceil((writtenLines / 100) / 3) - 1;
 alert (`Ви можете запізнитися ще ${remainingLateness} разів`);
+
+// Example 3
+let linesCode = Number(prompt('Введіть кількість рядків коду:'));
+let numberOfLateness = Number(prompt('Введіть кількість запізнень:'));
+let payment = Math.floor(linesCode / 100) * 50;
+if (numberOfLateness >= 3) {
+    payment -= (numberOfLateness - 2) * 20;
+} else if (payment < 0) {
+    payment = 0;
+}
+alert(`Вам заплатять ${payment}`);
